6a24840a7802a725c05fdc06c9fedbfd63e5391d
antiproton85 <kevinmcgr@gmail.com>
2020-08-12 06:09:22 -0400

    Remove the tester code - created a new test project for that purpose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import jsonpickle
 import logging
-import pprint
 import time
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -15,22 +13,6 @@ from sfdc.client import SFClient
 # import utility
 
 package_logger.initialize_logging()
-
-def test_bq():
-    bq_client = BQClient.from_config()
-
-    query_str = f"SELECT * FROM {bq_client.dataset_id}.{bq_client.table_id} WHERE username = 'mohammad.m.khalique@pwc.com' LIMIT 100"
-    # query_str = f"SELECT * FROM {bq_client.dataset_id}.df_pipeline_health_score LIMIT 1"
-
-    # query_str = f"SELECT COUNT(*) FROM {bq_client.dataset_id}.{bq_client.table_id} WHERE username > 'n' AND username <= 'szzz'"
-
-    bq_client.query(query_str)
-
-    pp = pprint.PrettyPrinter(indent=4)
-
-    for row in bq_client.results:
-        output_json = jsonpickle.encode(row.items(), unpicklable=False)
-        pp.pprint(output_json)
 
 def run_main():
     bq_client = BQClient.from_config()
@@ -91,14 +73,11 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-j", "--headless", help="Run system on an unattended schedule", dest="headless_flag",
                       default=None, action="store_true")
-    parser.add_option("-t", "--test", help="Test Import Mode", dest="test_flag", default=None, action="store_true")
 
     options, args = parser.parse_args()
 
     if options.headless_flag:
         run_sched()
-    elif options.test_flag:
-        test_bq()
     else:
         run_main()
 
