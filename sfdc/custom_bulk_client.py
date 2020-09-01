5458935edb135a78e09e239be3ffe78673611ec9
antiproton85 <kevinmcgr@gmail.com>
2020-09-01 07:28:25 -0400

    Refactor batch monitoring

--- sfdc/custom_bulk_client.py
+++ sfdc/custom_bulk_client.py
@@ -71,7 +71,24 @@ class SFBulkCustomClient:
     def send_bulk_update(self, records_for_update: list, sobject: str='Contact'):
         # create the batch job
         job_id = self.create_job_json('update', sobject=sobject)
-        self.job_queue[job_id] = False
+
+        job = None
+        if job_id not in self.job_queue:
+            job = {
+                "id": job_id,
+                "is_complete": 0,
+                "batch_total": 0,
+                "batch_count": 0,
+                "batch_complete_count": 0,
+                "records_total": 0,
+                "records_processed": 0,
+                "records_failed": 0
+            }
+        else:
+            job = self.job_queue.get(job_id)
+
+        job['records_total'] += len(records_for_update)
+
 
         if len(records_for_update) > self.batch_record_limit:
             # add batches to the job
@@ -81,54 +98,94 @@ class SFBulkCustomClient:
                 start = self.batch_record_limit * i
                 end = self.batch_record_limit * (i + 1) - 1
 
+                job['batch_total'] += 1
+
                 logging.info(f'Sending {sobject} batch {i + 1} - updating {len(records_for_update[start:end])} rows.')
                 self.add_batch_json(job_id, records_for_update[start:end])
         else:
             logging.info(f'Sending single {sobject} batch to job')
+            job['batch_total'] = 1
             self.add_batch_json(job_id, records_for_update)
 
         logging.info('Closing job...')
+        self.job_queue[job_id] = job
         self.close_job_json(job_id)
 
-    def check_bulk_job_complete(self, job_id: str):
+    def update_job_status(self, job_id: str):
         url = f"{self.bulk_url}job/{job_id}/batch"
 
         resp = requests.get(url, headers=self.headers_json).json()
 
-        is_complete = True
+        job_is_complete = True
+        job_records_processed = 0
+        job_records_failed = 0
+        job_batch_count_total = 0
+        job_batch_complete_total = 0
+
         for batch in resp['batchInfo']:
             batch_id = batch['id']
-            batch_status = batch['state'] == 'Completed' or batch['state'] == 'Failed'
+            batch_state = batch['state']
+            batch_status = batch_state == 'Completed' or batch['state'] == 'Failed'
+            batch_records_processed = batch['numberRecordsProcessed']
+            batch_records_failed = batch['numberRecordsFailed']
 
-            if 'Fail' in batch['state']:
+            job_batch_count_total += 1
+            job_records_processed += batch_records_processed
+            job_records_failed += batch_records_failed
+
+            if batch_status:
+                job_batch_complete_total += 1
+
+            if 'Fail' in batch_state:
                 logging.error(f'Failure detected for job.batch: {job_id}.{batch_id}.')
                 return True
 
-            logging.debug(f"batch id: {batch['id']} - state: {batch['state']} - "
-                         f"records processed: {batch['numberRecordsProcessed']} - "
-                         f"records failed: {batch['numberRecordsFailed']}")
-            is_complete = is_complete and batch_status
+            logging.debug(f"batch id: {batch_id} - state: {batch_state} - "
+                         f"records processed: {batch_records_processed} - "
+                         f"records failed: {batch_records_failed}")
 
-        return is_complete
+            job_is_complete = job_is_complete and batch_status
+
+        job_status = self.job_queue.get(job_id)
+        job_status['is_complete'] = job_is_complete
+        job_status['batch_count'] = job_batch_count_total
+        job_status['batch_complete_count'] = job_batch_complete_total
+        job_status['records_processed'] = job_records_processed
+        job_status['records_failed'] = job_records_failed
 
     def monitor_job_queue(self):
-        pending_count = 0
+        job_completed_count = 0
         queue_length = len(self.job_queue)
 
         # Only allow the monitor to run for 10 minutes.
         start_d = datetime.now()
         run_d = datetime.now()
-        while pending_count < queue_length and run_d < start_d + timedelta(minutes=15):
+        while job_completed_count < queue_length:
+            job_count = 1
+
             for job_id, status in self.job_queue.items():
-                if not status:
-                    job_status = self.check_bulk_job_complete(job_id)
+                self.update_job_status(job_id)
 
-                    if job_status:
-                        pending_count += 1
-                        self.job_queue[job_id] = job_status
+                r_t = status['records_total']
+                r_p = status['records_processed']
+                r_f = status['records_failed']
+                b_t = status['batch_total']
+                b_c = status['batch_count']
+                b_cp = status['batch_complete_count']
 
-            logging.info(f'{pending_count} of {queue_length} remaining.')
+                logging.info(f'Job: {job_count}/{queue_length} - batches: {b_cp}/{b_t} - records: {r_p}/{r_t}({r_f})')
+
+                if status['is_complete']:
+                    job_completed_count += 1
+
+                job_count += 1
+
+
             time.sleep(15)
             run_d = datetime.now()
 
+            if run_d < start_d + timedelta(minutes=15):
+                logging.error('Monitoring halted due to timeout')
+
         logging.info('All jobs complete.')
+        self.job_queue.clear()
