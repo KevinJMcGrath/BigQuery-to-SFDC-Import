9c2cd0155592090b0e7a764dba203984b9acb76d
Kevin <kevinmcgr@gmail.com>
2021-01-31 09:34:10 -0500

    Remove commented PB items (Removed from SFDC)

--- sfdc/pb_client.py
+++ sfdc/pb_client.py
@@ -5,19 +5,16 @@ import urllib.parse
 contact_pb_dict = {
     'AS_Premium_News_Content': 3,
     'Auto_Populate_Opportunity_Field_in_Contact_Record': 9,
-    # 'Contact_Owner_equals_AM_when_Inactive_user_is_Activated': 3,
     'Corp_Investor_Relations_PositionId_Update': 2,
     'MCO_Financial_Inst_Global_AS_TRUE': 2,
     'MCO_Fundamental_Global_AS_TRUE': 2,
     'Set_up_trial_config': 9,
     'Transcript_Type_Default': 2,
-    # 'Turn_of_direct_broker_feed': 2,
     'Turn_on_Moody_s_Credit_Research_Capital_Markets': 2,
     'Turn_on_underlying_MCO_Corporate_Global_AS_fields': 2
 }
 
 opp_pb_dict = {
-    #'Delete_Content_of_Opportunity_field_in_Contact_if_Contact_Role_is_Deleted': None,
     'Assign_primary_contact': 3,
     'Opp_Stage_Date_Stamps': 3,
     'Update_Opportunity_Intacct_Entity': 1,
